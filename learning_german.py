1ef56882d5b80e67ba69c6ff91b015c485f7ebdf
Ana Paula Gomes <apgomes88@gmail.com>
2020-04-07 15:48:26 +0200

    Black it

--- learning_german.py
+++ learning_german.py
@@ -34,29 +34,25 @@ else:
 
 def send_message(to, from_, message):
     try:
-        sent_message = client.messages.create(
-            to=to,
-            from_=from_,
-            body=message
-        )
+        sent_message = client.messages.create(to=to, from_=from_, body=message)
     except TwilioRestException as e:
-        logging.error(f'Oh no: {e}')
+        logging.error(f"Oh no: {e}")
         return
 
     return sent_message.sid
 
 
 def send_a_german_word():
-    german_words = json.load(open('most-common-german-words.json'))
+    german_words = json.load(open("most-common-german-words.json"))
     chosen_word = random.choice(german_words)
-    if chosen_word['part_of_speech']:
+    if chosen_word["part_of_speech"]:
         part_of_speech = f"({chosen_word['part_of_speech']})"
     else:
-        part_of_speech = ''
-    if chosen_word['english_translation']:
+        part_of_speech = ""
+    if chosen_word["english_translation"]:
         meaning = f"\n\nMeaning: {chosen_word['english_translation']}"
     else:
-        meaning = ''
+        meaning = ""
     message = (
         f"The word of the day is... "
         f"{chosen_word['german_word'].upper()} {part_of_speech} {meaning}"
@@ -66,5 +62,5 @@ def send_a_german_word():
     return sid
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     send_a_german_word()
--- test_learning_german.py
+++ test_learning_german.py
@@ -4,15 +4,17 @@ from twilio.base.exceptions import TwilioRestException
 from unittest import mock
 
 
-@mock.patch('learning_german.client.messages.create')
-def test_send_a_common_word(mock_create):
-    words_set = [{
-        "rank": 105,
-        "german_word": "wirklich",
-        "english_translation": "real, true, natural",
-        "part_of_speech": "adjective",
-        "url": "http://languagedaily.com/learn-german/vocabulary/common-german-words-3"
-    }]
+@mock.patch("learning_german.client.messages.create")
+def test_send_a_common_word(create_message_mock):
+    words_set = [
+        {
+            "rank": 105,
+            "german_word": "wirklich",
+            "english_translation": "real, true, natural",
+            "part_of_speech": "adjective",
+            "url": "http://languagedaily.com/learn-german/vocabulary/common-german-words-3",
+        }
+    ]
     message = (
         f"The word of the day is... {words_set[0]['german_word']}"
         f" ({words_set[0]['part_of_speech']})"
@@ -20,44 +22,48 @@ def test_send_a_common_word(mock_create):
         f"\nMore at: {words_set[0]['url']}"
     )
 
-    expected_sid = 'SM87105da94bff44b999e4e6eb90d8eb6a'
-    mock_create.return_value.sid = expected_sid
+    expected_sid = "SM87105da94bff44b999e4e6eb90d8eb6a"
+    create_message_mock.return_value.sid = expected_sid
 
     to = "+491112223"
     from_ = "+493332221"
     sid = send_message(to, from_, message)
 
-    assert mock_create.called is True
+    assert create_message_mock.called is True
     assert sid == expected_sid
 
 
 @pytest.mark.skip
-@mock.patch('learning_german.client.messages.create')
-def test_raise_exception_when_cannot_send_a_message(mock_create):
+@mock.patch("learning_german.client.messages.create")
+def test_raise_exception_when_cannot_send_a_message(create_message_mock):
     error_message = (
-        f"Unable to create record: The 'To' number " 
+        f"Unable to create record: The 'To' number "
         "+491799999999 is not a valid phone number."
     )
     status = 500
-    uri = '/Accounts/ACXXXXXXXXXXXXXXXXX/Messages.json'
+    uri = "/Accounts/ACXXXXXXXXXXXXXXXXX/Messages.json"
     msg = error_message
-    mock_create.side_effect = TwilioRestException(status, uri, msg=error_message)
+    create_message_mock.side_effect = TwilioRestException(
+        status, uri, msg=error_message
+    )
     with pytest.raises(TwilioRestException):
         to = "+491112223"
         from_ = "+493332221"
         send_message(to, from_, "Wrong message")
 
 
-@mock.patch('learning_german.client.messages.create')
-def test_log_error_when_cannot_send_a_message(mock_create, caplog):
+@mock.patch("learning_german.client.messages.create")
+def test_log_error_when_cannot_send_a_message(create_message_mock, caplog):
     error_message = (
-        f"Unable to create record: The 'To' number " 
+        f"Unable to create record: The 'To' number "
         "+491799999999 is not a valid phone number."
     )
     status = 500
-    uri = '/Accounts/ACXXXXXXXXXXXXXXXXX/Messages.json'
+    uri = "/Accounts/ACXXXXXXXXXXXXXXXXX/Messages.json"
     msg = error_message
-    mock_create.side_effect = TwilioRestException(status, uri, msg=error_message)
+    create_message_mock.side_effect = TwilioRestException(
+        status, uri, msg=error_message
+    )
 
     to = "+491112223"
     from_ = "+493332221"
